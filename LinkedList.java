fc2e94066575c3741c1e12fcb5a7edabfe77044c
xyuzuki <lindaliu@Yuzuki.local>
2017-03-05 17:06:00 -0500

    Node Class
    Created Node class

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,24 @@
 public class LinkedList
 {
-  
+  private class Node
+  {
+    
+    String nodes;
+    Node next;
+    Node before;
+    
+    Node(String x, Node y, Node z)
+    {
+      nodes = x;
+      next = y;
+      before = z;
+    }
+    
+    Node(String x)
+    {
+      this(x, null, null);
+    }
+        
+  }
   
 }
