713ab9fabb3f668fab83c6023c7aa2b2a6a71bae
xyuzuki <lindaliu@dyn-169-226-91-202.wireless.albany.edu>
2017-03-22 10:52:18 -0400

    Finish
    deleteNode/add

--- LinkedList.java
+++ LinkedList.java
@@ -68,6 +68,8 @@ public class LinkedList
     return count;
   }
   
+  
+  
   public void add(String data)
   {
     if(isEmpty())
@@ -79,6 +81,74 @@ public class LinkedList
     {
       last.next = new Node(data, null, last);
         last = last.next;
+    }
+  }
+  
+  public void add(int index, String data)
+  {
+    if( index < 0 || index > size())
+    {
+      System.out.println("The index is outside of the bounds.");
+      return;
+    }
+    if(index == 0)
+    {
+      Node prevFirst = first;
+      first = new Node(data, prevFirst, null);
+      
+      if(prevFirst != null)
+      {
+        prevFirst.before = first;
+      }
+      if(last == null)
+      {
+        last = first;
+      }
+      return;
+    }
+    
+    
+    Node before1 = first;
+    
+    for(int x = 1; x <= index - 1; x++)
+    {
+      before1 = before1.next;
+    }
+    
+    Node after1 = before1.next;
+    Node middle = new Node(data, after1, before1);
+    after1.next = middle;
+    
+    if(after1 == null)
+    {
+      last = middle;
+    }
+    else
+    {
+      after1.before = middle;
+    }
+  }
+  
+  public void alpha(String data)
+  {
+    if(isEmpty())
+    {
+      last = new Node(data);
+      first = last;
+    }
+    else
+    {
+      int index = 0;
+      
+      Node different = first;
+      
+      while(different.nodes.compareTo(data) < 0)
+      {
+        index++;
+        different = different.next;
+      }
+      
+      this.add(index, data);
     }
   }
   
@@ -99,6 +169,49 @@ public class LinkedList
     }
     return null;
   }
+  
+  public void deleteNode(String data)
+  {
+    if(isEmpty())
+    {
+      System.out.println("Empty list");
+    }
+    
+    Node deleteNode = first;
+    
+    while(deleteNode != null && !deleteNode.nodes.equals(data))
+    {
+      deleteNode = deleteNode.next;
+    }
+    
+    
+    if(deleteNode == null)
+    {
+      System.out.println("There is no node to delete.");
+    }
+    
+    Node before1 = deleteNode.before;
+    Node after1 = deleteNode.next;
+    
+    if(before1 == null)
+    {
+      first = after1;
+    }
+    else 
+    {
+      before1.next = after1;
+    }
+    
+    if(after1 == null)
+    {
+      last = before1;
+    }
+    else
+    {
+      after1.before = before1;
+    }
+  }
+  
   
   public void traverseForward()
   {
@@ -127,5 +240,8 @@ public class LinkedList
     this.first = null;
     this.last = null;
   }
+  
+  
+  
   
 }
