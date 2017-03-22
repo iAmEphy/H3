1c68e97515bd598ca15eb5ae4e405acd16578718
xyuzuki <lindaliu@dyn-169-226-91-202.wireless.albany.edu>
2017-03-22 11:05:44 -0400

    isEmpty
    test

--- Tester.java
+++ Tester.java
@@ -2,7 +2,21 @@ public class Tester
 {
   public static void main(String []args)
   {
+    LinkedList test = new LinkedList();
     
+    System.out.println("The " + test + " is empty: " + test.isEmpty() + "\n");
+    
+    System.out.println("Adding 1, 2, 3, 4 to the list");
+    test.add("1");
+    test.add("2");
+    test.add("3");
+    test.add("4");
+    
+    System.out.println("The " + test + " is empty: " + test.isEmpty() + "\n");
+    
+    
+    System.out.println("Printed forward: ");
+    test.traverseForward();
     
   }
 }
